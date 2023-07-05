feb5cbca2b011728e96f407749f04fc91bae32c3
mozqueda-alejandro <admozqueda@nevada.unr.edu>
2023-07-05 00:37:32 -0700

    Day 1 Completed

---
+++ 01-introduction/datatypes.js
@@ -0,0 +1,27 @@
+let string = 'Hello World';
+let number = 1;
+let bool = true;
+let array = [1, 2, 3];
+let undefinedVariable = undefined;
+let nullVariable = null;
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof bool);
+console.log(typeof array);
+console.log(typeof undefinedVariable);
+console.log(typeof nullVariable);
+
+let firstName;
+let lastName;
+let maritalStatus;
+let country;
+let age;
+
+let firstName_1, lastName_1, maritalStatus_1, country_1, age_1;
+
+let myAge = 25;
+let yourAge = 30;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
---
+++ 01-introduction/variable.js
@@ -0,0 +1,9 @@
+let string;
+let bool;
+let undefinedVariable;
+let nullVariable;
+
+string = 'Hello World';
+bool = true;
+undefinedVariable = undefined;
+nullVariable = null;
